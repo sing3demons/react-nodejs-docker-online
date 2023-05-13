830ddae15e5120e6280e8bdbd8fe59ca7560f171
sing3demons <sing3demons@live.com>
2023-05-13 21:49:48 +0700

    add Material , Material Icons

--- react-mui-strapi/reactapp/src/main.tsx
+++ react-mui-strapi/reactapp/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from '@emotion/react'
+import theme from './config/theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 )
